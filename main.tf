11cf174b04f5cc9c5d5220ba6dfd04afc315d16f
Nihad Abdul Nazar <90106684+NihadAbdulNazar@users.noreply.github.com>
2025-11-18 21:01:04 +0530

    Add DynamoDB table, S3 and EC2 instance resources

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_s3_bucket" "tf_state" {
   bucket = "nihad-terraform-state"
 }
 
+
 resource "aws_dynamodb_table" "tf_lock" {
   name         = "terraform-locks"
   hash_key     = "LockID"
@@ -10,5 +11,15 @@ resource "aws_dynamodb_table" "tf_lock" {
   attribute {
     name = "LockID"
     type = "S"
+  }
+}
+
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0dee22c13ea7a9a67" # Amazon Linux 2023 (ap-south-1)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Nihad-EC2"
   }
 }
